a27a0a4c2558e19ef94da06b2c7ac4c1143e2e12
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2020-09-02 13:48:00 +0200

    restructure code and add code for create_todo

--- todo_api/todo_api_stack.py
+++ todo_api/todo_api_stack.py
@@ -7,6 +7,14 @@ from aws_cdk import (
 )
 
 
+class PythonFunction(aws_lambda.Function):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        kwargs["handler"] = "main.handler"
+        kwargs["runtime"] = aws_lambda.Runtime.PYTHON_3_8
+
+        super().__init__(scope, id, **kwargs)
+
+
 class TodoApiStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
@@ -29,22 +37,10 @@ class TodoApiStack(core.Stack):
             )
         )
 
-        # Lambda functions
-        list_todos_function = aws_lambda.Function(self, "ListTodosFunction",
-            handler="main.handler",
-            runtime=aws_lambda.Runtime.PYTHON_3_8,
-            code=aws_lambda.Code.asset("src/list_todos")
-        )
-        create_todo_function = aws_lambda.Function(self, "CreateTodoFunction",
-            handler="main.handler",
-            runtime=aws_lambda.Runtime.PYTHON_3_8,
-            code=aws_lambda.Code.asset("src/create_todo")
-        )
-        get_todo_function = aws_lambda.Function(self, "GetTodoFunction",
-            handler="main.handler",
-            runtime=aws_lambda.Runtime.PYTHON_3_8,
-            code=aws_lambda.Code.asset("src/get_todo")
-        )
+        # Lambda functions environment
+        lambda_environment = {
+            "TABLE_NAME": table.table_name
+        }
 
         # REST Api
         api = aws_apigateway.RestApi(self, "TodoApi")
@@ -52,9 +48,33 @@ class TodoApiStack(core.Stack):
         api_todos_todo = api_todos.add_resource(r"{todo}")
 
         # GET /todos
+        list_todos_function = PythonFunction(self, "ListTodosFunction",
+            code=aws_lambda.Code.asset("src/list_todos"),
+            environment=lambda_environment
+        )
+        table.grant_read_data(list_todos_function)
         api_todos.add_method("GET", aws_apigateway.LambdaIntegration(list_todos_function))
+
         # POST /todos
+        create_todo_function = PythonFunction(self, "CreateTodoFunction",
+            code=aws_lambda.Code.asset("src/create_todo"),
+            environment=lambda_environment
+        )
+        table.grant_write_data(create_todo_function)
         api_todos.add_method("POST", aws_apigateway.LambdaIntegration(create_todo_function))
+
         # GET /todos/{todo}
+        get_todo_function = PythonFunction(self, "GetTodoFunction",
+            code=aws_lambda.Code.asset("src/get_todo"),
+            environment=lambda_environment
+        )
+        table.grant_read_data(get_todo_function)
         api_todos_todo.add_method("GET", aws_apigateway.LambdaIntegration(get_todo_function))
 
+        # DELETE /todos/{todo}
+        delete_todo_function = PythonFunction(self, "DeleteTodoFunction",
+            code=aws_lambda.Code.asset("src/delete_todo"),
+            environment=lambda_environment
+        )
+        table.grant_write_data(delete_todo_function)
+        api_todos_todo.add_method("DELETE", aws_apigateway.LambdaIntegration(delete_todo_function))
