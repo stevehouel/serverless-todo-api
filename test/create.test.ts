6db448ce2e68e50e3034aa9b81dd4006fb9579a2
Nicolas Moutschen <moutschn@amazon.com>
2020-09-17 11:29:15 +0200

    fix: integration tests

--- test/create.test.ts
+++ test/create.test.ts
@@ -1,61 +1,34 @@
 const AWS = require('aws-sdk');
+AWS.config.update({region:'eu-west-1'});
 const axios = require('axios');
+import { aws4Interceptor } from "aws4-axios";
+
 const apiEndpoint = process.env.TODO_API_ENDPOINT;
-const userPoolId = process.env.TODO_USER_POOL;
-const userPoolClientId = process.env.TODO_USER_POOL_CLIENT;
 
 test('create todo', async () => {
-  const cognito = new AWS.CognitoIdentityServiceProvider();
-  // TODO: Generate random values
-  const userName = 'test-username';
-  const password = 'super-secret-password';
+  const credentials = new AWS.SharedIniFileCredentials();
 
-  // Create a temporary cognito user
-  cognito.adminCreateUser({
-    UserPoolId: userPoolId,
-    Username: userName,
-    MessageAction: 'SUPPRESS',
+  const interceptor = aws4Interceptor({
+    region: "eu-west-1",
+    service: "execute-api"
+  }, {
+    accessKeyId: credentials.accessKeyId,
+    secretAccessKey: credentials.secretAccessKey
   });
-  cognito.adminSetUserPassword({
-    UserPoolId: userPoolId,
-    Username: userName,
-    Password: password,
-    Permanent: true,
-  });
-
-  // Retrieve a JWT token
-  const cognitoResponse = await cognito.adminInitiateAuth({
-    UserPoolId: userPoolId,
-    ClientId: userPoolClientId,
-    AuthFlow: 'ADMIN_NO_SRP_AUTH',
-    AuthParameters: {
-      USERNAME: userName,
-      PASSWORD: password,
-    },
-  }).promise();
-  const jwtToken = cognitoResponse.AuthenticationResult.IdToken;
+  
+  axios.interceptors.request.use(interceptor);
 
   // Create a new TODO
   const todoCreateRequest = {
     title: 'Create a new todo',
     content: 'This is a test of a new todo',
   };
-  const createResponse = await axios.post(apiEndpoint + '/todos', todoCreateRequest, { headers: {
-    Authorization: jwtToken,
-  }});
+  const createResponse = await axios.post(apiEndpoint + '/todos', todoCreateRequest);
 
   // Retrieve the TODO
-  const getResponse = await axios.get(apiEndpoint + '/todos/' + createResponse.data, { headers: {
-    Authorization: jwtToken,
-  }});
+  const getResponse = await axios.get(apiEndpoint + '/todos/' + createResponse.data);
 
   expect(getResponse.status).toBe(200);
   expect(getResponse.data.title).toBe(todoCreateRequest.title);
   expect(getResponse.data.content).toBe(todoCreateRequest.content);
-
-  // Delete the temporary user
-  cognito.adminDeleteUser({
-    UserPoolId: userPoolId,
-    Username: userName,
-  });
 });
