d0c76bb274961d8edaf1cd2fcf3d8b1d212ce10f
Nicolas Moutschen <moutschn@amazon.com>
2020-09-23 16:54:46 +0200

    test: add getAll test

--- test/create.test.ts
+++ test/create.test.ts
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 AWS.config.update({region:'eu-west-1'});
 const axios = require('axios');
-import { aws4Interceptor } from "aws4-axios";
+import { aws4Interceptor } from 'aws4-axios';
 
 const apiEndpoint = process.env.TODO_API_ENDPOINT;
 
@@ -9,8 +9,8 @@ test('create todo', async () => {
   const credentials = new AWS.SharedIniFileCredentials();
 
   const interceptor = aws4Interceptor({
-    region: "eu-west-1",
-    service: "execute-api"
+    region: 'eu-west-1',
+    service: 'execute-api'
   }, {
     accessKeyId: credentials.accessKeyId,
     secretAccessKey: credentials.secretAccessKey
@@ -23,9 +23,12 @@ test('create todo', async () => {
     title: 'Create a new todo',
     content: 'This is a test of a new todo',
   };
+  console.log('POST Address', apiEndpoint + '/todos');
   const createResponse = await axios.post(apiEndpoint + '/todos', todoCreateRequest);
+  console.log('Response', createResponse.data);
 
   // Retrieve the TODO
+  console.log('GET Address', apiEndpoint + '/todos/' + createResponse.data);
   const getResponse = await axios.get(apiEndpoint + '/todos/' + createResponse.data);
 
   expect(getResponse.status).toBe(200);
