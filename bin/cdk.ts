9ca3fe0c685ffa4e92334f8d742dedf3d1fd5936
EC2 Default User <ec2-user@ip-172-31-3-16.eu-west-1.compute.internal>
2020-12-15 21:13:56 +0000

    change domain and owner

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -9,14 +9,14 @@ const app = new cdk.App();
 new PipelineStack(app, 'ServerlessTodoApi-PipelineStack', {
     repositoryName: "serverless-todo-api",
     branchName: "main",
-    ownerName: "nmoutschen"
+    ownerName: "vikingen13"
 });
 
 const domainName = app.node.tryGetContext("domainName");
 
 // Implement Infra Stage for developer environment
 new InfraStage(app, 'ServerlessTodoApi-Dev', {
-    domainName: domainName ? domainName : 'fr-houes',
+    domainName: domainName ? domainName : 'fr-grazzini',
     callbackUrls: ['http://localhost', 'http://localhost:3000'],
     env: {
         account: process.env.CDK_DEFAULT_ACCOUNT,
